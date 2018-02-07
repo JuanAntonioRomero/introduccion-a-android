bbefa750bd13bc1d4e71c5e986e95161d29fb3a7
Juan Antonio Romero Gómez <jromegom@everis.com>
2018-02-07 18:52:46 +0100

    Update for Layout types

--- app/src/main/java/com/example/everis/android/introduccionaandroid/MainActivity.java
+++ app/src/main/java/com/example/everis/android/introduccionaandroid/MainActivity.java
@@ -3,98 +3,56 @@ package com.example.everis.android.introduccionaandroid;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import com.example.everis.android.introduccionaandroid.layouts.FrameLayoutActivity;
+import com.example.everis.android.introduccionaandroid.layouts.GridLayoutActivity;
+import com.example.everis.android.introduccionaandroid.layouts.LinearLayoutActivity;
+import com.example.everis.android.introduccionaandroid.layouts.RelativeLayoutActivity;
 
-    private static final String TAG = "MainActivity";
+public class MainActivity extends Activity implements View.OnClickListener {
 
-    private TextView mLeftTextView;
-    private TextView mCenterTextView;
-    private TextView mRightTextView;
-    private ImageView mImageView;
-    private Button mButton;
-    private Button mNextScreenButton;
+    private Button mFrameLayoutButton;
+    private Button mLinearLayoutButton;
+    private Button mGridLayoutButton;
+    private Button mRelativeLayoutButton;
 
-    /*
-    Este método se llama una sola vez por instancia, cada vez que se crea la pantalla.
-     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.v(TAG, "onCreate");
 
-        mLeftTextView =findViewById(R.id.text_left);
-        mCenterTextView =findViewById(R.id.text_center);
-        mRightTextView =findViewById(R.id.text_right);
-        mImageView =findViewById(R.id.imageView);
-        mButton = findViewById(R.id.button);
-        mNextScreenButton= findViewById(R.id.button_nextScreen);
+        mFrameLayoutButton = findViewById(R.id.button_frameLayout);
+        mLinearLayoutButton = findViewById(R.id.button_linearLayout);
+        mGridLayoutButton = findViewById(R.id.button_gridLayout);
+        mRelativeLayoutButton = findViewById(R.id.button_relativeLayout);
 
-        mButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mCenterTextView.setText(R.string.text_center_alt);
-                mImageView.setImageResource(R.drawable.if_food_c240_2427880);
-            }
-        });
-
-        mNextScreenButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent (MainActivity.this, AnotherActivity.class);
-                startActivity(intent);
-            }
-        });
+        mFrameLayoutButton.setOnClickListener(this);
+        mLinearLayoutButton.setOnClickListener(this);
+        mGridLayoutButton.setOnClickListener(this);
+        mRelativeLayoutButton.setOnClickListener(this);
     }
 
-    /*
-    Este método se llama cada vez que la pantalla se va a mostrar al usuario.
-     */
     @Override
-    protected void onStart() {
-        super.onStart();
-        Log.v(TAG, "onStart");
-    }
+    public void onClick(View view) {
+        Class targetClass = null;
 
-    /*
-    Este método se llama cada vez que la pantalla va a empezar a recibir interacción por parte del usuario.
-     */
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.v(TAG, "onResume");
-    }
+        if (view == mFrameLayoutButton) {
+            targetClass = FrameLayoutActivity.class;
+        }
+        else if (view == mLinearLayoutButton) {
+            targetClass = LinearLayoutActivity.class;
+        }
+        else if (view == mGridLayoutButton) {
+            targetClass = GridLayoutActivity.class;
+        }
+        else if (view == mRelativeLayoutButton) {
+            targetClass = RelativeLayoutActivity.class;
+        }
 
-    /*
-    Este método se llama cada vez que la pantalla deja de estar visible.
-     */
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.v(TAG, "onPause");
-    }
+        Intent intent = new Intent(this, targetClass);
+        startActivity(intent);
 
-    /*
-    Este método se llama cada vez que la instancia vuelve a iniciarse tras haberse llamado a onPause.
-     */
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.v(TAG, "onRestart");
-    }
-
-    /*
-    Este método se llama una sola vez por instancia, cuando el sistema destruye la Activity. Puede no llamarse.
-     */
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.v(TAG, "onDestroy");
     }
 }
---
+++ app/src/main/java/com/example/everis/android/introduccionaandroid/layouts/RelativeLayoutActivity.java
@@ -0,0 +1,19 @@
+package com.example.everis.android.introduccionaandroid.layouts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.everis.android.introduccionaandroid.R;
+
+/**
+ * Created by jromegom on 07/02/2018.
+ */
+
+public class RelativeLayoutActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_layout);
+    }
+}
